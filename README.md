edbf89c65856135e856685ee5e07a0681c52b020
HugeErick <erick.parada101@gmail.com>
2026-01-13 16:29:02 -0600

    feat: refactor & file dialog support

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # rust-imgui-dockable-template
 
+[![Rust](https://img.shields.io/badge/Rust-1.8%2B-orange)](https://rust-lang.org/)
+[![imgui-rs](https://img.shields.io/badge/imgui-rs-blue)](https://github.com/imgui-rs/imgui-rs)
+[![License](https://img.shields.io/badge/License-MIT-green)](LICENSE)
+
+
 ## Description
 
 Finally a working dear imgui rust with docking feature enable template
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,46 @@
 mod support;
+use rfd::FileDialog;
+use std::path::PathBuf;
 
 fn main() {
-    let system = support::init(file!());
-    system.main_loop(move |_, ui| {
-        ui.dockspace_over_main_viewport();
-        ui.window("Hello world")
-            .size([300.0, 100.0], imgui::Condition::FirstUseEver)
-            .build(|| {
-                ui.text("Hello world!");
-                ui.separator();
-                let mouse_pos = ui.io().mouse_pos;
-                ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
-            });
-    });
+  let system = support::init(file!());
+
+  let mut selected_path: Option<PathBuf> = None;
+
+  system.main_loop(move |_, ui| {
+    ui.dockspace_over_main_viewport();
+    ui.window("Hello world")
+      .size([300.0, 300.0], imgui::Condition::FirstUseEver)
+      .build(|| {
+        ui.text("Hello world!");
+        ui.separator();
+        let mouse_pos = ui.io().mouse_pos;
+        ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
+        ui.separator();
+
+
+        // file dialog
+        if ui.button("Select file") && let Some(path) = FileDialog::new().pick_file() {
+          selected_path = Some(path);
+        }
+
+
+        // folder dialog
+        if ui.button("Select Folder") && let Some(path) = FileDialog::new().pick_folder() {
+          selected_path = Some(path);
+        }
+
+        // display dialogs result
+        if let Some(ref path) = selected_path {
+          ui.text(format!("Selected: {}", path.display()));
+          if ui.button("Clear") {
+            selected_path = None;
+          }
+        } else {
+          ui.text("No file or folder selected.");
+        }
+
+      });
+
+  });
 }
